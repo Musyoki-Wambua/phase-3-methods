99fe4f2a4e5b77ca513a54c6fb772eb0fa7d3090
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-06 21:40:11 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+
+# method 1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# method 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# method 3
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+# method 4
+def add(num1, num2)
+    num1 + num2
+end
+
+# method 5
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num / 2
+end
+
+halve(6)
